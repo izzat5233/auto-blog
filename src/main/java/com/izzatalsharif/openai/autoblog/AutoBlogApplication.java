dce07a242f8fd778bac625fdb0f0a5054eb90b2a
izzat5233 <izzat5233@gmail.com>
2023-06-11 23:40:39 +0300

    Initial commit

---
+++ src/main/java/com/izzatalsharif/openai/autoblog/AutoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.izzatalsharif.openai.autoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/izzatalsharif/openai/autoblog/AutoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzatalsharif.openai.autoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
