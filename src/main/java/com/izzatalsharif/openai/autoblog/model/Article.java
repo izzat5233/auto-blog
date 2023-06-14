bc770744c45d19a46ad031c2566cb27b227adf06
izzat5233 <izzat5233@gmail.com>
2023-06-14 20:55:18 +0300

    Updated models

--- src/main/java/com/izzatalsharif/openai/autoblog/model/Article.java
+++ src/main/java/com/izzatalsharif/openai/autoblog/model/Article.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -21,11 +22,20 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(name = "title", nullable = false, unique = true)
+    @Column(name = "keywords", nullable = false, unique = true)
+    private String keywords;
+
+    @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content", columnDefinition = "TEXT")
-    private String content;
+    @Column(name = "introduction", nullable = false, columnDefinition = "TEXT")
+    private String introduction;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Section> sections;
+
+    @Column(name = "conclusion", nullable = false, columnDefinition = "TEXT")
+    private String conclusion;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createAt;
---
+++ src/main/java/com/izzatalsharif/openai/autoblog/model/Section.java
@@ -0,0 +1,44 @@
+package com.izzatalsharif.openai.autoblog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "section")
+public class Section {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_section_id")
+    private Section parentSection;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "parentSection", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Section> subsections;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+}
